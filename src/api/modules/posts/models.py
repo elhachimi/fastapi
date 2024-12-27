5f1935ecbccf42fabaae042466ff823a4f1bab30
Wael El Hachimi <v-whachimi@digitalocean.com>
2024-12-27 23:36:05 +0100

    adds jwt authentication

--- src/api/modules/posts/models.py
+++ src/api/modules/posts/models.py
@@ -1,15 +1,36 @@
+import uuid
 from datetime import datetime
 from typing import Optional
 
 from sqlmodel import SQLModel, Field, Relationship
 
+from src.api.modules.users.models import User, UserPublic
 
-class Post(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+# Shared properties
+
+
+class PostBase(SQLModel):
     title: str
     description: Optional[str] = Field(default=None)
-    owner_id: int = Field(foreign_key="user.id")
-    user: "User" = Relationship(back_populates="posts")
     published: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: Optional[datetime] = Field(default=None)
+
+
+# Database model
+
+
+class Post(PostBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    owner_id: uuid.UUID = Field(
+        foreign_key="user.id", nullable=False, ondelete="CASCADE"
+    )
+    user: User = Relationship(back_populates="posts")
+
+
+# Properties returned via API
+
+
+class PostPublic(PostBase):
+    user: UserPublic
